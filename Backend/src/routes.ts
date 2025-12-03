3678106ecd3018af0ec94b8fd3079738f84cb78b
Soarezzsemj <popcornor6topster@outlook.com>
2025-12-03 01:26:10 -0300

    feat: front end

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -6,6 +6,11 @@ const router = Router();
 
 router.post("/summary", summarryController);
 
+router.get("/ping", (req, res) => {
+  res.json({ ok: true, msg: "API funcionando!" });
+});
+
+
 
 
 export default router;
